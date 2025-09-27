f3f3c96038e6271d7fc15f9dc8d51acfd81b3ca9
user <fake@email.com>
2025-09-26 22:19:51 -0700

    Add rss feed fetcher

---
+++ internal/rss/feed.go
@@ -0,0 +1,59 @@
+package rss
+
+import (
+	"context"
+	"encoding/xml"
+	"html"
+	"io"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func FetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
+	var feed *RSSFeed
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, feedUrl, nil)
+	if err != nil {
+		return feed, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return feed, err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return feed, err
+	}
+
+	err = xml.Unmarshal(body, &feed)
+	if err != nil {
+		return feed, err
+	}
+
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	return feed, nil
+}
